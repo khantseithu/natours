4dd3b40d871320cbe4ebb4a6aacf62c8e3fc9e81
khantseithu <khantseithuu@gmail.com>
2023-03-10 16:28:52 +0630

    handle delete

--- app.js
+++ app.js
@@ -74,6 +74,19 @@ app.patch("/api/v1/tours/:id", (req, res) => {
   });
 });
 
+app.delete("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
