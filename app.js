47bb164e2c040a20eed3e7bd6d18be5b9fe3495d
khantseithu <khantseithuu@gmail.com>
2023-03-10 16:15:02 +0630

    handle unexisted route

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.get("/api/v1/tours/:id", (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
   res.status(200).json({
     status: "success",
     results: tours.length,
