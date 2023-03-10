99ae291b44521f1d9235d78f85045c358c5da4c6
khantseithu <khantseithuu@gmail.com>
2023-03-10 16:26:06 +0630

    handle patch

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf-8")
 );
 
-app.get("/api/v1/tours/:id", (req, res) => {
+app.get("/api/v1/tours/:id?", (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
@@ -61,7 +61,18 @@ app.post("/api/v1/tours", (req, res) => {
   );
 });
 
-app.patch("/api/v1/tours:id");
+app.patch("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    tour: "<Updated tour here..>",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
