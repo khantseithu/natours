fd856e1ab2f52035b29e110ac5d052af2058b695
khantseithu <khantseithuu@gmail.com>
2023-03-10 16:16:40 +0630

    refactor

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.get("/api/v1/tours/:id", (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
-  if (id > tours.length) {
+  //   if (id > tours.length) {
+  if (!tour) {
     return res.status(404).json({
       status: "fail",
       message: "Invalid ID",
