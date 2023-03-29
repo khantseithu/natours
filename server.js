2ea2f75e8a6009b6e262384571439c6524c41ae4
Khant Sithu <khantseithuu@gmail.com>
2023-03-29 08:25:19 +0000

    refactoring into mvc

--- server.js
+++ server.js
@@ -14,36 +14,6 @@ mongoose.connect(DB, {
   useFindAndModify: false
 }).then(() => console.log('DB connection successful!')) // return a promise
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String, 
-    required: [true, "a tour must have a name"],
-    unique: true
-  },
-   
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, "a tour must have a price"]
-  }
-})
-
-const Tour = mongoose.model('Tour', tourSchema)
-
-const testTour = new Tour({
-  name: "The Forest Hiker",
-  rating: 4.7,
-  price: 497
-})
-
-testTour.save()
-  .then((document)=> console.log(document))
-  .catch(err => console.log("ERROR: ", err))
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
