915e43a36eeae4bf825daa1dbc45dd1d159a6724
Khant Sithu <khantseithuu@gmail.com>
2023-03-29 07:59:17 +0000

    create a document to test the model

--- server.js
+++ server.js
@@ -17,19 +17,32 @@ mongoose.connect(DB, {
 const tourSchema = new mongoose.Schema({
   name: {
     type: String, 
-    required: [true, "a tour must have a name"]},
-    unique: true,
+    required: [true, "a tour must have a name"],
+    unique: true
+  },
+   
   rating: {
     type: Number,
     default: 4.5
   },
   price: {
     type: Number,
-    required: [true, "a tour must have a value"]
+    required: [true, "a tour must have a price"]
   }
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
+
+const testTour = new Tour({
+  name: "The Forest Hiker",
+  rating: 4.7,
+  price: 497
+})
+
+testTour.save()
+  .then((document)=> console.log(document))
+  .catch(err => console.log("ERROR: ", err))
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
