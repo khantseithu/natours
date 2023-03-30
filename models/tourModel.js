b6325e6361ddb9044fab95da0958f22de5ff7c2a
Khant Sithu <khantseithuu@gmail.com>
2023-03-30 05:53:49 +0000

    add createdAt timestamp

--- models/tourModel.js
+++ models/tourModel.js
@@ -67,7 +67,7 @@ const tourSchema = new mongoose.Schema({
     createdAt: {
       type: Date,
       default: Date.now(),
-      select: false
+      // select: false
     },
     startDates: [Date]
   })
