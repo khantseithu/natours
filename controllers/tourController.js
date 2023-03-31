978ad7d8894c1a26084563b10f5885eed1b55d48
Khant Sithu <khantseithuu@gmail.com>
2023-03-31 15:46:27 +0000

    refactor using apiFeatures obj

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,59 +1,27 @@
-const fs = require("fs");
 const Tour = require('./../models/tourModel')
+const APIFeatures = require('./../utils/apiFeatures');
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, "utf-8")
-// );
-
-// exports.checkID = (req, res, next, val) => {
-//   console.log(`Tour id is: ${val}`);
-//   if (req.params.id * 1 > tours.length) {
-//     return res.status(404).json({
-//       status: "fail",
-//       message: "Invalid ID",
-//     });
-//   }
-//   next();
-// };
-
-// DON'T need this middleware anymore cuz of the model in mongoDb
-// exports.checkBody = (req, res, next) => {
-//   if (!req.body.name || !req.body.price) {
-//     return res.status(400).json({
-//       status: "fail",
-//       message: "Missing name or price",
-//     });
-//   }
-//   next();
-// };
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+}
 
 
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query)
+    // console.log(req.query)
     // EXECUTE QUERY
-    // const features = new APIFeatures(Tour.find(), req.query)
-    //   .filter()
-    //   .sort()
-    //   .limitFields()
-    //   .paginate();
-    // const tours = await features.query;
-    // const tours = await Tour.find({
-    //   duration: "5",
-    //   difficulty: "easy"
-    // });
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
-    // Filtering
-    const queryObj = {...req.query};
-    const excludedFields = ['page', 'limit', 'sort', 'fields']
-    excludedFields.forEach(el => delete queryObj[el])
 
-    // Advanced filtering
-    let queryStr = JSON.stringify(queryObj)
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr))
-
-    const tours = await Tour.find(JSON.parse(queryStr))
+    
     // SEND RESPONSE
     res.status(200).json({
       status: 'success',
@@ -146,4 +114,77 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+
+
+
+
+
+
+
+// --FOR REFERENCE
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, "utf-8")
+// );
+
+// exports.checkID = (req, res, next, val) => {
+//   console.log(`Tour id is: ${val}`);
+//   if (req.params.id * 1 > tours.length) {
+//     return res.status(404).json({
+//       status: "fail",
+//       message: "Invalid ID",
+//     });
+//   }
+//   next();
+// };
+
+// DON'T need this middleware anymore cuz of the model in mongoDb
+// exports.checkBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: "fail",
+//       message: "Missing name or price",
+//     });
+//   }
+//   next();
+// };
+
+
+// const tours = await Tour.find({
+    //   duration: "5",
+    //   difficulty: "easy"
+    // });
+
+    // Filtering
+    // arse(queryStr))const queryObj = {...req.query};
+    // const excludedFields = ['page', 'limit', 'sort', 'fields']
+    // excludedFields.forEach(el => delete queryObj[el])
+
+    // // Advanced filtering
+    // let queryStr = JSON.stringify(queryObj)
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+
+    // let query = await Tour.find(JSON.p
+
+    //2) Sorting
+    // if(req.query.sort){
+    //   const sortBy = req.query.sort.split(',').join(' ');
+    //   console.log(sortBy)
+    //   query = query.sort(sortBy)
+    // }
+    //  else {
+    //   query = query.sort('-createdAt')
+    // }
+
+    // 3) Fields limiting
+    // if(req.query.fields){
+    //   const fields = req.query.fields.split(',').join(' ');
+    //   query = query.select(fields)
+    // } else {
+    //   query = query.select('-__v')
+    // }
+
+    //EXECUTE QUERY
+    // const tours = await query;
 
