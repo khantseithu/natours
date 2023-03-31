121060c7d25dfaee85a5ade16009635e9c35174e
Khant Sithu <khantseithuu@gmail.com>
2023-03-31 15:47:49 +0000

    added aliasTopTours

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,6 +10,9 @@ router.param("id", (req, res, next, val) => {
 // router.param("id", tourController.checkID);
 
 router
+  .get(tourController.aliasTopTours, tourController.getAllTours)
+
+router
   .route("/")
   .get(tourController.getAllTours)
   .get(tourController.getTour)
